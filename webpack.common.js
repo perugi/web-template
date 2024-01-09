807217efabb507b9873f90bb84cca75e1e88d807
perugi <dominik.perusko@gmail.com>
2024-01-09 11:31:08 +0100

    Favicon plugin mode set to auto for faster compilation in development.

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,37 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      inject: "body",
+      template: './src/index.html',
+      inject: 'body',
     }),
-    new FaviconsWebpackPlugin("./src/img/logo.png"),
+    new FaviconsWebpackPlugin({
+      logo: './src/img/logo.png',
+      mode: 'light',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
